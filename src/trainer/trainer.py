b7b291007e25a052dcf3290302b7bea13c04ca85
vorobeevich <valerygegelevsky@yandex.ru>
2023-07-20 18:26:51 +0300

    Add test logging

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -150,7 +150,7 @@ class Trainer:
         )
 
         with open(f"{self.checkpoint_dir}logs.txt", "w") as f:
-            print("F1 history:", file=f)
+            print("Start training. F1 history:", file=f)
 
         self.init_training()
         train_loader, val_loader, test_loader = self.create_loaders()
@@ -178,6 +178,10 @@ class Trainer:
         self.load_checkpoint()
         test_metrics = self.inference_epoch_model(test_loader)
         logging.info(f"ERM training. Results on test: {test_metrics}")
+        with open(f"{self.checkpoint_dir}logs.txt", "a") as f:
+            print(f"Finish training. Test f1: {test_metrics['f1']}. Test accuracy: {test_metrics['accuracy']}. \
+                Test precision: {test_metrics['precision']}. Test recall: {test_metrics['recall']}", file=f)
+
 
     def save_checkpoint(self):
         state = {
