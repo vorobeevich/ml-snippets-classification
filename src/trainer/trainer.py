162c659bc1b486c0ccb8ee518dd94903a93a94dd
vorobeevich <valerygegelevsky@yandex.ru>
2023-07-21 21:42:25 +0300

    Add batch logs

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -77,9 +77,9 @@ class Trainer:
             self.optimizer.step()
             self.scheduler.step()
             loss_sum += loss.item() * input_ids.shape[0]
-            true_labels += labels.tolist()
+            true_labels += labels.tolist()фп
             pred_labels += logits.argmax(dim=-1).tolist()
-
+            pbar.set_description("Accuracy on batch %f loss on batch %f" % ((logits.argmax(dim=-1) == labels).sum().item() / labels.shape[0]).item(), loss.item())
         res = self.calculate_metrics(true_labels, pred_labels)
         res["loss"] = loss_sum / len(loader.dataset)
         return res
@@ -97,6 +97,7 @@ class Trainer:
                 loss_sum += loss.item() * input_ids.shape[0]
                 true_labels += labels.tolist()
                 pred_labels += logits.argmax(dim=-1).tolist()
+                pbar.set_description("Accuracy on batch %f loss on batch %f" % ((logits.argmax(dim=-1) == labels).sum().item() / labels.shape[0]).item(), loss.item())
 
         res = self.calculate_metrics(true_labels, pred_labels)
         res["loss"] = loss_sum / len(loader.dataset)
